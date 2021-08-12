da521bd7acd559f9a60a7c0a9b1f66631183dd05
Heemaeng <gml7814@gmail.com>
2021-08-12 22:16:42 +0900

    CreateUser.js 생성

---
+++ src/CreateUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateUser({ username, email, onChange, onCreate }) {
+    console.log('CreateUser');
+    return (
+        <div>
+            <input/>
+            <input/>
+            <button></button>
+        </div>
+    )
+}
