bc6e1431ea24906a90d60ceeaa89beac80445a15
Heemaeng <gml7814@gmail.com>
2021-08-13 00:44:30 +0900

    react 배운데 까지 컴파일

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -4,9 +4,21 @@ function CreateUser({ username, email, onChange, onCreate }) {
     console.log('CreateUser');
     return (
         <div>
-            <input/>
-            <input/>
-            <button></button>
+            <input
+                name="username"
+                placeholder="계정명"
+                onChange={onChange}
+                value={username}
+            />
+            <input
+                name="email"
+                placeholder="이메일"
+                onChange={onChange}
+                value={email}
+            />
+            <button onClick={onCreate}>등록</button>
         </div>
     )
 }
+
+export default React.memo(CreateUser);
