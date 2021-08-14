5f935c008308f098fb053ad88ce5e0a9ddac17fb
Heemaeng <gml7814@gmail.com>
2021-08-14 23:19:47 +0900

    함수 컴포넌트 -> 클래스 컴포넌트

--- src/Hello.js
+++ src/Hello.js
@@ -1,17 +1,33 @@
-import React from 'react';
-// 세미콜론 생략 하셔도 되고 안하셔도 됨
-function Hello({ color, name, isSpecial }) {
-    return (
-        <div style={{
-            color
-        }}>
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        </div>
-    );
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    static defaultProps = {
+        name: '이름없음',
+    };
+    render() {
+        const { color, isSpecial, name } = this.defaultProps;
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        )
+    }
 }
 
-Hello.defaultProps = {
-    name:'이름없음'
-};
+// 세미콜론 생략 하셔도 되고 안하셔도 됨
+// function Hello({ color, name, isSpecial }) {
+//     return (
+//         <div style={{
+//             color
+//         }}>
+//             {isSpecial && <b>*</b>}
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
+
+// Hello.defaultProps = {
+//     name:'이름없음'
+// };
 export default Hello;
